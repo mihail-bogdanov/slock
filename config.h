f333b3d847956da92363005817cf6f1172611291
mihail-bogdanov <mihabog0720@gmail.com>
2021-11-23 17:36:24 +0100

    added all files

---
+++ config.h
@@ -0,0 +1,25 @@
+/* user and group to drop privileges to */
+static const char *user  = "mihail";
+static const char *group = "mihail";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#131C26",   /* during input */
+	[FAILED] = "#FF6E67",   /* wrong password */
+	[CAPS] = "FF6E67",         /* CapsLock on */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 1;
+
+/* default message */
+static const char * message = "Lo schermo e' bloccato. Immettere la Password.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso10646-1";
